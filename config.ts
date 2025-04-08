1dedb2724ad519850c239cfcc840903715cbda8d
Efrizal <efrizalsy0424@gmail.com>
2025-04-08 09:56:45 +0700

    Update config.ts

--- config.ts
+++ config.ts
@@ -3,13 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config({ path: `./${process.env.NODE_ENV ? '.' + process.env.NODE_ENV : ''}.env` });
 
 export const config = {
-	port: process.env.PORT || 3000,
     baseUrl: process.env.BASE_URL || "https://dev.sitesched.com",
     credentials: {
         email: process.env.EMAIL || "",
         password: process.env.PASSWORD || "",
     },
     projectId: process.env.PROJECT_ID || "",
-    mAuthToken: process.env.MAUTH_TOKEN || "",
-    mAuthUser:  process.env.MAUTH_USER || ""
 };
